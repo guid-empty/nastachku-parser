ac0b9fc379004ae9b445d46a0fad4142543b098b
Andrey Smirnov <andrey.smirnov@team.wrike.com>
2019-04-04 12:02:20 +0300

    initial

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:html/dom.dart';
 import 'package:html/parser.dart';
-import 'package:http/http.dart'; // Contains DOM related classes for extracting data from elements
+import 'package:http/http.dart';
 
 void main(List<String> arguments) async {
   await initiate();
@@ -22,63 +22,73 @@ Future initiate() async {
           .readAsStringSync();
 
   var document = parse(body);
-  List<Element> roomHeaders = document.querySelectorAll('div.program__header-wrapper > div.program__header-item');
-  for (Element roomHeader in roomHeaders) {
-    final roomLiter = clearText(roomHeader.nodes[1].text);
-    final roomName = clearText(roomHeader.nodes[2].text);
-    print('$roomLiter, $roomName');
+  final dateSelectors = const ['26-04-2019', '27-04-2019'];
 
-    rooms.add(RoomDescription(roomLiter, roomName));
-  }
+  for (String dateSelector in dateSelectors) {
+    print(
+        '----------------------------------------- current date is $dateSelector ---------------------------------------------');
 
-  List<Element> programColumns = document.querySelectorAll('div.program__body > div.program__column');
-  for (int index = 0; index < programColumns.length; index++) {
-    final room = rooms[index];
-    Element programColumn = programColumns[index];
+    final Element dateContent = document.querySelector('div#content_$dateSelector');
+    final List<Element> roomHeaders =
+        dateContent.querySelectorAll('div.program__header-wrapper > div.program__header-item');
+    for (Element roomHeader in roomHeaders) {
+      final roomLiter = clearText(roomHeader.nodes[1].text);
+      final roomName = clearText(roomHeader.nodes[2].text);
+      print('$roomLiter, $roomName');
 
-    print('----------------------------------------- lectures in $room ---------------------------------------------');
-    //  events
-    List<Element> eventNodes = programColumn.querySelectorAll('div.program__event');
-    for (Element eventNode in eventNodes) {
-      final eventTime = clearText(eventNode.querySelector('div.program__item-time')?.nodes?.first?.text);
-      final eventName = clearText(eventNode.querySelector('div.program__item-name')?.text);
-
-      if (eventTime != null && eventName != null) {
-        print('event name: $eventName on time: $eventTime');
-      } else if (eventTime == null && eventName != null) {
-        print('event name: $eventName');
-      }
+      rooms.add(RoomDescription(roomLiter, roomName));
     }
 
-    //  lectures
-    List<Element> lectureNodes = programColumn.querySelectorAll('a.program__item-lecture');
-    for (Element lectureNode in lectureNodes) {
-      final programTypeClassName =
-          lectureNode.classes.firstWhere((className) => className.startsWith('program__type'), orElse: () => null);
+    final List<Element> programColumns = dateContent.querySelectorAll('div.program__body > div.program__column');
+    for (int index = 0; index < programColumns.length; index++) {
+      final room = rooms[index];
+      Element programColumn = programColumns[index];
 
-      final lectureType = programTypeClassName == null ? 'unknown' : programTypeClassName.split('-')[1];
-      final lectureTimeNode = lectureNode.querySelector('div.program__item-time').nodes[0];
-      final lectureSpeakerNodes = lectureNode.querySelectorAll('div.program__item-name');
-      final lectureSpeakerJobNodes = lectureNode.querySelectorAll('div.program__item-jobs');
+      print(
+          '----------------------------------------- lectures in $room ---------------------------------------------');
+      //  events
+      List<Element> eventNodes = programColumn.querySelectorAll('div.program__event');
+      for (Element eventNode in eventNodes) {
+        final eventTime = clearText(eventNode.querySelector('div.program__item-time')?.nodes?.first?.text);
+        final eventName = clearText(eventNode.querySelector('div.program__item-name')?.text);
 
-      final lectureTime = clearText(lectureTimeNode.text);
-      print('lectureType: $lectureType, lectureTime: $lectureTime');
+        if (eventTime != null && eventName != null) {
+          print('event name: $eventName on time: $eventTime');
+        } else if (eventTime == null && eventName != null) {
+          print('event name: $eventName');
+        }
+      }
 
-      lectureSpeakerNodes.forEach((e) {
-        final lectureSpeaker = clearText(e.text);
+      //  lectures
+      List<Element> lectureNodes = programColumn.querySelectorAll('a.program__item-lecture');
+      for (Element lectureNode in lectureNodes) {
+        final programTypeClassName =
+            lectureNode.classes.firstWhere((className) => className.startsWith('program__type'), orElse: () => null);
 
-        print('speaker: $lectureSpeaker');
-      });
+        final lectureType = programTypeClassName == null ? 'unknown' : programTypeClassName.split('-')[1];
+        final lectureTimeNode = lectureNode.querySelector('div.program__item-time').nodes[0];
+        final lectureSpeakerNodes = lectureNode.querySelectorAll('div.program__item-name');
+        final lectureSpeakerJobNodes = lectureNode.querySelectorAll('div.program__item-jobs');
 
-      lectureSpeakerJobNodes.forEach((e) {
-        final lectureSpeakerJob = clearText(e.text);
+        final lectureTime = clearText(lectureTimeNode.text);
+        print('lectureType: $lectureType, lectureTime: $lectureTime');
 
-        print('speaker job: $lectureSpeakerJob');
-      });
+        lectureSpeakerNodes.forEach((e) {
+          final lectureSpeaker = clearText(e.text);
 
-      final lectureDescription = clearText(lectureNode.querySelector('div.program__item-desc').text);
-      print('lecture: $lectureDescription');
-      print('\n');
+          print('speaker: $lectureSpeaker');
+        });
+
+        lectureSpeakerJobNodes.forEach((e) {
+          final lectureSpeakerJob = clearText(e.text);
+
+          print('speaker job: $lectureSpeakerJob');
+        });
+
+        final lectureDescription = clearText(lectureNode.querySelector('div.program__item-desc').text);
+        print('lecture: $lectureDescription');
+        print('\n');
+      }
     }
   }
 }
